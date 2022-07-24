fc936467ca3bb8acfed77420028ed5bd3ce6241a
Ask3wQQ <47623394+Ask3wQQ@users.noreply.github.com>
2022-07-24 18:42:58 +0800

    [update] remote theme: minimal-mistakes

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "中文測試",
+        "excerpt":"這是部落格的第一篇文章！  tag: blog-test   h2標題  h3標題  終於利用 jekyll 將 Minimal Mistakes 主題建構起來囉！ 並且也成功的建立了 _posts 資料夾，把這個文章放進去。   jekyll 看到 _posts 資料夾後，就會把裡面的文件轉換成文章啦！   等等再來試看看把 Minimal Mistakes 整個資料夾 push 到 GitHub 的部落格專案目錄會有什麼效果。   中文測試   the firest article   ","categories": [],
+        "tags": ["blog-test"],
+        "url": "/%E4%B8%AD%E6%96%87%E6%B8%AC%E8%A9%A6/",
+        "teaser": null
+      }]
